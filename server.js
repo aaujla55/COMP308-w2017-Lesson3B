0c90cb243a071b90e5a684a0f111a7b67c817b32
aaujla55 <amandeep99555@gmail.com>
2017-01-28 14:28:20 -0500

    Procfile added, express website

--- server.js
+++ server.js
@@ -2,13 +2,20 @@
 
 // import the 3rd party connect module and
 // assign it as a reference to the connect variable
-let connect = require('connect');
+let connect = require('express');
 
 // create an instance of the connect server - app
-let app = connect();
+let app = express();
 
 // create the port constant
 const port = 3000;
+
+/**
+ * Get port from environment and store in Express.
+ */
+let port = process.env.PORT || localport;
+app.set('port', port);
+
 
 // start listening on the port
 app.listen(port);
